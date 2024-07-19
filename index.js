91e9c12a9f57d8bae615a7c9f39970733df06c1b
denismnjima <denismnjima@gmail.com>
2024-07-19 15:33:32 +0300

    solved question

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// index.js
+document.addEventListener('DOMContentLoaded', () => {
+    const paragraph = document.querySelector('p');
+    if (paragraph) {
+      paragraph.textContent = "This is really cool!";
+    }
+  });
+  
